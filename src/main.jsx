2a1faf3e21fde061f76214f4f29b77b6979e739e
Sourav O <o.sourav5@gmail.com>
2026-01-07 14:45:31 +0530

    lenis

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import Lenis from "lenis";
 import "./index.css";
 import App from "./App.jsx";
 import AuthProvider from "./context/AuthContext.jsx";
+
+// Initialize Lenis smooth scroll
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  orientation: "vertical",
+  smoothWheel: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
